6c3c05e6bd18e1b5d2b4a71a01169a15402e323f
Nannan <nanna7077@gmail.com>
2023-02-22 13:29:13 +0530

    Formatting

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,9 +1,9 @@
-fetch('static/data.json')
+fetch('static/data.json', {cache: "no-store"})
 .then(resp=>resp.json())
 .then(
     (resp) => {
         Object.keys(resp.announcements).forEach((k) => {
-            document.getElementById('announcementsparent').innerHTML += `<span><a style="color: #fff; text-decoration: none;" href="${resp.announcements[k].link}">${resp.announcements[k].title}&nbsp;&nbsp;|&nbsp;&nbsp;</a></span>`;
+            document.getElementById('announcementsparent').innerHTML += `<span><a style="color: #fff; text-decoration: none;" href="${resp.announcements[k].link}" target="_blank">${resp.announcements[k].title}&nbsp;&nbsp;|&nbsp;&nbsp;</a></span>`;
         })
     }
 )
